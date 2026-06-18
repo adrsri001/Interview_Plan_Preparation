1634853e21cec54261d58c8ff3c767dc6790dfa8
Adarsh Shrivastav <adarshshrivastav08@gmail.com>
2026-06-18 16:34:49 +0530

    Update CORS origin to production URL

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://interview-plan-preparation-1.onrender.com",
     credentials: true
 }))
 
